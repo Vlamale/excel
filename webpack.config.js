2c6590bb1147c6eb60d5ff02b88023fbfe16de14
Vlamale <kristoyt@bk.ru>
2021-04-10 14:14:08 +0300

    Finish progect config

---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production'
+  const filename = ext =>
+  isProd ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`
+
+  const plagins = () => {
+    const base = [
+      new HtmlWebpackPlugin({
+        template: './index.html'
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'src', 'favicon.ico'),
+            to: path.resolve(__dirname, 'dist')
+          },
+        ],
+      }),
+      new MiniCssExtractPlugin({
+        filename: filename('css')
+      })
+    ]
+    if (!isProd) {
+      base.push(new ESLintPlugin())
+    }
+    return base
+  }
+
+  return {
+    target: 'web',
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+      main: './index.js'
+    },
+    output: {
+      filename: filename('js'),
+      path: path.resolve(__dirname, 'dist'),
+      clean: true
+    },
+    resolve: {
+      extensions: [
+        '.js'
+      ],
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@core': path.resolve(__dirname, 'src', 'core')
+      }
+    },
+    devServer: {
+      port: 3000,
+      hot: true
+    },
+    devtool: !isProd ? 'source-map' : false,
+    plugins: plagins(),
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ],
+    },
+  }
+}
