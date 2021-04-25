5f29b764e02de92926f7fbcf8fb24af6c6c160e4
Vlamale <kristoyt@bk.ru>
2021-04-25 16:08:43 +0300

    Finish base table logic

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,17 +1,21 @@
 import {$} from '../../core/dom'
+import {Emmiter} from '../../core/Emmiter'
 
 export class Excel {
   constructor(selector, options) {
     this.$el = $(selector)
     this.components = options.components || []
+    this.emmiter = new Emmiter()
   }
 
   getRoot() {
     const $root = $.create('div', 'excel')
 
+    const componentOptions = {emmiter: this.emmiter}
+
     this.components = this.components.map( Component => {
       const $el = $.create('div', Component.className)
-      const component = new Component($el)
+      const component = new Component($el, componentOptions)
 
       $el.html(component.toHTML())
       $root.append($el)
@@ -23,5 +27,9 @@ export class Excel {
   render() {
     this.$el.append(this.getRoot())
     this.components.forEach(component => component.init())
+  }
+
+  destroy() {
+    this.components.forEach(component => component.destroy())
   }
 }
--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -3,10 +3,11 @@ import {ExcelComponent} from '../../core/ExcelComponent'
 export class Toolbar extends ExcelComponent {
   static className = 'excel__toolbar'
 
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
       name: 'Toolbar',
-      listeners: ['click']
+      listeners: [],
+      ...options
     })
   }
 
@@ -36,9 +37,5 @@ export class Toolbar extends ExcelComponent {
         <i class="material-icons">format_underlined</i>
       </div>
     `
-  }
-
-  onClick(event) {
-    console.log('dge');
   }
 }
